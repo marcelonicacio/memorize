36de267bba8961705d5fbdee36a5590056c995db
marcelonicacio <marcelonicacio@me.com>
2025-04-17 18:45:03 +0100

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Marcelo Nicacio on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
